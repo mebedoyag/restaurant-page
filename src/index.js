786fbeb2faf1475456902be9083dbe5a74c3530e
Miguel Bedoya <me.bedoya10@gmail.com>
2021-11-13 12:30:47 -0500

    load page ok

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('OMG it works');
+import { home } from './page-load';
+import Backgroun from './restaurant.jpg';
+
+function component() {
+  const container = document.getElementById('content');
+
+  return home(container, Backgroun);
+}
+
+document.body.appendChild(component());
+
